f76da21322d96392860246d9dd65b0858edc862e
aCiotola <alessandromciotola@gmail.com>
2018-01-12 10:46:37 -0500

    Add Main/Sub Description FXML / Remove old Forms

---
+++ src/main/java/com/ciotola/controller/MainDescriptionFXMLController.java
@@ -0,0 +1,107 @@
+package com.ciotola.controller;
+
+import com.ciotola.entities.MainDescription;
+import com.ciotola.persistence.AccountingDAOImp;
+import com.ciotola.persistence.IAccountingDAO;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainDescriptionFXMLController
+{
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getName());  
+    private IAccountingDAO accountDAO;
+    
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="mdField"
+    private TextField mdField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="mdTable"
+    private TableView<MainDescription> mdTable; // Value injected by FXMLLoader
+
+    @FXML // fx:id="mdNumField"
+    private TableColumn<MainDescription, Integer> mdNumColumn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="mdNameField"
+    private TableColumn<MainDescription, String> mdNameColumn; // Value injected by FXMLLoader
+
+    @FXML
+    void onDeleteMainDescription(ActionEvent event) throws SQLException 
+    {
+        if(!mdField.getText().equals(""))
+        {     
+            int id = accountDAO.findMainDescriptionByName(mdField.getText()).getMainDescriptionID();
+            accountDAO.deleteMainDescription(id);
+            mdField.setText("");
+            
+            log.debug("Main Description deleted!");            
+            displayTable();
+        }
+        else        
+            mdField.setText("Please select a Main Description!");
+    }
+
+    @FXML
+    void onSaveMainDescription(ActionEvent event) throws SQLException 
+    {
+        if(!mdField.getText().equals(""))
+        {            
+            MainDescription mainDescription = new MainDescription();     
+            mainDescription.setMainDescriptionName(mdField.getText());
+            accountDAO.addMainDescription(mainDescription);
+            
+            log.debug("Main Description created!");
+            
+            mdField.setText("");
+            
+            displayTable();
+        }
+        else        
+            mdField.setText("Please enter a description!");
+    }
+
+    public MainDescriptionFXMLController()
+    {
+        super();
+    }
+    
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() throws SQLException, IOException
+    {
+        assert mdField != null : "fx:id=\"mdField\" was not injected: check your FXML file 'MainDescriptionFXML.fxml'.";
+        assert mdTable != null : "fx:id=\"mdTable\" was not injected: check your FXML file 'MainDescriptionFXML.fxml'.";
+        assert mdNumColumn != null : "fx:id=\"mdNumField\" was not injected: check your FXML file 'MainDescriptionFXML.fxml'.";
+        assert mdNameColumn != null : "fx:id=\"mdNameField\" was not injected: check your FXML file 'MainDescriptionFXML.fxml'.";
+
+        accountDAO = new AccountingDAOImp();
+        displayTable();
+        
+        mdNumColumn.setCellValueFactory(cellData -> cellData.getValue().getMainDescriptionIDProperty().asObject());
+        mdNameColumn.setCellValueFactory(cellData -> cellData.getValue().getMainDescriptionNameProperty());     
+        mdTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showMainDescriptionDetails(newValue));
+    }
+    
+    public void displayTable() throws SQLException
+    {
+        mdTable.setItems(accountDAO.findAllMainDescriptions());
+    }
+    
+    private void showMainDescriptionDetails(MainDescription md) 
+    {
+        if(md != null)        
+            mdField.setText(md.getMainDescriptionName());
+    }
+}
---
+++ src/main/java/com/ciotola/controller/SubDescriptionFXMLController.java
@@ -0,0 +1,107 @@
+package com.ciotola.controller;
+
+import com.ciotola.entities.SubDescription;
+import com.ciotola.persistence.AccountingDAOImp;
+import com.ciotola.persistence.IAccountingDAO;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SubDescriptionFXMLController
+{
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getName());  
+    private IAccountingDAO accountDAO;
+    
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="sdNameField"
+    private TextField sdNameField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="sdTable"
+    private TableView<SubDescription> sdTable; // Value injected by FXMLLoader
+
+    @FXML // fx:id="sdNumColumn"
+    private TableColumn<SubDescription, Integer> sdNumColumn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="sdNameColumn"
+    private TableColumn<SubDescription, String> sdNameColumn; // Value injected by FXMLLoader
+
+    @FXML
+    void onDeleteSubDescription(ActionEvent event) throws SQLException
+    {
+        if(!sdNameField.getText().equals(""))
+        {     
+            int id = accountDAO.findSubDescriptionByName(sdNameField.getText()).getSubDescriptionID();
+            accountDAO.deleteSubDescription(id);
+            sdNameField.setText("");
+            
+            log.debug("Sub Description deleted!");            
+            displayTable();
+        }
+        else        
+            sdNameField.setText("Please select a Sub Description!");
+    }
+
+    @FXML
+    void onSaveSubDescription(ActionEvent event) throws SQLException 
+    {
+        if(!sdNameField.getText().equals(""))
+        {            
+            SubDescription subDescription = new SubDescription();     
+            subDescription.setSubDescriptionName(sdNameField.getText());
+            accountDAO.addSubDescription(subDescription);
+            
+            log.debug("Sub Description created!");
+            
+            sdNameField.setText("");
+            
+            displayTable();
+        }
+        else        
+            sdNameField.setText("Please enter a description!");
+    }
+
+    public SubDescriptionFXMLController()
+    {
+        super();
+    }
+    
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() throws IOException, SQLException
+    {
+        assert sdNameField != null : "fx:id=\"sdNameField\" was not injected: check your FXML file 'SubDescriptionFXML.fxml'.";
+        assert sdTable != null : "fx:id=\"sdTable\" was not injected: check your FXML file 'SubDescriptionFXML.fxml'.";
+        assert sdNumColumn != null : "fx:id=\"sdNumColumn\" was not injected: check your FXML file 'SubDescriptionFXML.fxml'.";
+        assert sdNameColumn != null : "fx:id=\"sdNameColumn\" was not injected: check your FXML file 'SubDescriptionFXML.fxml'.";
+
+        accountDAO = new AccountingDAOImp();
+        displayTable();
+        
+        sdNumColumn.setCellValueFactory(cellData -> cellData.getValue().getSubDescriptionIDProperty().asObject());
+        sdNameColumn.setCellValueFactory(cellData -> cellData.getValue().getSubDescriptionNameProperty());     
+        sdTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showSubDescriptionDetails(newValue));
+    }
+    
+    public void displayTable() throws SQLException
+    {
+        sdTable.setItems(accountDAO.findAllSubDescriptions());
+    }
+    
+    private void showSubDescriptionDetails(SubDescription sd) 
+    {
+        if(sd != null)        
+            sdNameField.setText(sd.getSubDescriptionName());
+    }
+}
